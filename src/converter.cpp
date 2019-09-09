b8a4af8dc426015791bd9878d61bdf47bf770936
HiroIshida <spitfire.docomo@gmail.com>
2019-09-10 05:48:20 +0900

    finish converter

--- src/converter.cpp
+++ src/converter.cpp
@@ -1,6 +1,9 @@
 #include <ros/ros.h>
+#include <ros/package.h>
 #include <pcl/PCLPointCloud2.h>
 #include <sensor_msgs/PointCloud2.h>
+#include <sensor_msgs/PointCloud.h>
+#include <geometry_msgs/Point32.h>
 #include <pcl_conversions/pcl_conversions.h> // japanese version
 
 #include <pcl/point_cloud.h>
@@ -8,10 +11,13 @@
 
 #include "geometry_msgs/Point.h" 
 #include <iostream>
+#include <fstream>
 #include <vector>
+#include <string>
 
 #define COUNTOF(array) (sizeof(array) / sizeof(array[0]))
 #define PRINT(somthing) std::cout << somthing << std::endl;
+using namespace std;
 
 ros::Publisher pub;
 void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& msg_input)
@@ -20,37 +26,25 @@ void cloud_cb (const sensor_msgs::PointCloud2ConstPtr& msg_input)
   // tutorial in Japanese is wrong (using depricated header)  
   pcl::PointCloud<pcl::PointXYZ> cloud;
   pcl::fromROSMsg(*msg_input, cloud); 
-  float x_sum, y_sum, z_sum;
-  x_sum = y_sum = z_sum = 0;
-  int i = 0;
-  
-  // https://vml.sakura.ne.jp/koeda/PCL/tutorials/html/basic_structures.html#basic-structures
-  int N_valid = 0;
-  for(int i=0; i<cloud.points.size(); i++){
-    float x = cloud.points[i].x;
-    float y = cloud.points[i].y;
-    float z = cloud.points[i].z;
-    if(!(std::isnan(x) || std::isnan(y) || std::isnan(z))){
-        x_sum += x;
-        y_sum += y;
-        z_sum += z;
-        N_valid++;
-    }
+  sensor_msgs::PointCloud msg_pc;
+  msg_pc.header = msg_input->header;
+  for(int i = 0; i < cloud.points.size(); i++){
+    geometry_msgs::Point32 pt;
+    pt.x = cloud.points[i].x;
+    pt.y = cloud.points[i].y;
+    pt.z = cloud.points[i].z;
+    msg_pc.points.push_back(pt);
   }
-  auto point_msgs = geometry_msgs::Point();
-  point_msgs.x = x_sum/N_valid;
-  point_msgs.y = y_sum/N_valid;
-  point_msgs.z = z_sum/N_valid;
-  PRINT(z_sum/N_valid);
-  pub.publish(point_msgs);
+  pub.publish(msg_pc);
 }
 
 int main (int argc, char** argv)
 {
   ros::init (argc, argv, "pcl_center");
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe ("input", 1, cloud_cb);
-  pub = nh.advertise<geometry_msgs::Point> ("output", 1);
+  ros::Subscriber sub = nh.subscribe("/kinect_head/depth_registered/points", 1, cloud_cb);
+  pub = nh.advertise<sensor_msgs::PointCloud>("/kinect_converted", 1);
   ros::spin ();
 }
+
 

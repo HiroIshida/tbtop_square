075b719d13da0c2756f7747288d9831a4d2cace1
HiroIshida <spitfire.docomo@gmail.com>
2019-09-11 09:16:04 +0900

    publish image_msg

--- script/main.py
+++ script/main.py
@@ -3,7 +3,7 @@ import rospy
 import rospkg
 import csv
 import matplotlib.pyplot as plt
-from sensor_msgs.msg import PointCloud2
+from sensor_msgs.msg import Image
 from geometry_msgs.msg import Point
 from tbtop_square.msg import Projected
 from math import *
@@ -14,6 +14,7 @@ import time
 
 from detect_square import detect_rect
 import cv2
+from cv_bridge import CvBridge
 
 class MyQueue:
     def __init__(self, N):
@@ -33,14 +34,19 @@ class SquareDetector:
     def __init__(self, n_ave = 10):
         self.sub = rospy.Subscriber("input", Projected, self.callback)
         self.pub = rospy.Publisher("output", Point, queue_size = 10)
+        self.pub_img = rospy.Publisher("/tbtop_debug_image", Image)
         self.s_queue = MyQueue(n_ave)
 
     def callback(self, msg):
         x1 = np.array(msg.x_array.data)
         x2 = np.array(msg.y_array.data)
         x = np.vstack((x1, x2))
-        s_est_, size = detect_rect(x)
-        #print "s-est:" + str(s_est_)
+        s_est_, size, img_ = detect_rect(x)
+
+        img = cv2.cvtColor(img_, cv2.COLOR_GRAY2BGR)
+        bridge = CvBridge()
+        msg = bridge.cv2_to_imgmsg(img, 'bgr8')
+        self.pub_img.publish(msg)
 
         isInvalid = (size < 0.005 ** 2 or s_est_[2] == 0.0)
         if not isInvalid:

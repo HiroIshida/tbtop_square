b8ebd0eac3185f9fad26448afe3654673251607e
HiroIshida <spitfire.docomo@gmail.com>
2019-12-31 03:35:23 +0900

    use ave queue

--- script/main_circle.py
+++ script/main_circle.py
@@ -15,7 +15,7 @@ from cv_bridge import CvBridge
 class MyQueue:
     def __init__(self, N):
         self.N = N
-        self.data = [np.zeros(3) for n in range(N)]
+        self.data = [np.zeros(2) for n in range(N)]
 
     def push(self, elem):
         tmp = self.data[1:self.N]
@@ -23,13 +23,14 @@ class MyQueue:
         self.data = tmp
 
     def mean(self):
-        s_est_lst = [np.mean(np.array([s[i] for s in self.data])) for i in range(3)]
+        s_est_lst = [np.mean(np.array([s[i] for s in self.data])) for i in range(2)]
         return np.array(s_est_lst)
 
 class CircleDetector:
-    def __init__(self, n_ave = 10):
+    def __init__(self, n_ave = 5):
         self.sub = rospy.Subscriber("/cloud2d_projected", Projected, self.callback)
         self.pub = rospy.Publisher("/circle_center", Point, queue_size = 1)
+        self.s_queue = MyQueue(n_ave)
 
     def callback(self, msg):
         print("hoge")
@@ -37,11 +38,13 @@ class CircleDetector:
         x2 = np.array(msg.y_array.data)
         x = np.vstack((x1, x2))
         x_mean = np.mean(x, axis = 1)
-        x_mean_lst = list(x_mean)
-        print(x_mean_lst)
+        self.s_queue.push(x_mean)
+
+
 
         msg = Point()
-        msg.x, msg.y = x_mean_lst
+        x_averaged = list(self.s_queue.mean())
+        msg.x, msg.y = x_averaged
         self.pub.publish(msg)
 
 if __name__=='__main__':

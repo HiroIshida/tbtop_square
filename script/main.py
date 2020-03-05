d50b2e2bd795235ebeb707b050bedf89f9a02b61
HiroIshida <spitfire.docomo@gmail.com>
2020-03-06 01:13:46 +0900

    fixed S1

--- script/main.py
+++ script/main.py
@@ -21,9 +21,10 @@ from cv_bridge import CvBridge
 import tf
 
 class MyQueue:
-    def __init__(self, N):
+    def __init__(self, N, n):
         self.N = N
-        self.data = [np.zeros(3) for n in range(N)]
+        self.n = n
+        self.data = [np.zeros(self.n) for i in range(N)]
 
     def push(self, elem):
         tmp = self.data[1:self.N]
@@ -31,8 +32,31 @@ class MyQueue:
         self.data = tmp
 
     def mean(self):
-        s_est_lst = [np.mean(np.array([s[i] for s in self.data])) for i in range(3)]
-        return np.array(s_est_lst)
+        s_est_arr = np.vstack(self.data)
+        s_est = np.mean(s_est_arr, axis=0)
+        #s_est_lst = [np.mean(np.array([s[i] for s in self.data])) for i in range(self.N)]
+        return np.array(s_est)
+
+class S1Queue:
+    def __init__(self, N):
+        self.N = N
+        self.data = [0 for n in range(N)]
+        self.scale = 4
+
+    def push(self, angle):
+        tmp = self.data[1:self.N]
+        tmp.append(angle)
+        self.data = tmp
+
+    def mean(self):
+        angles4 = np.array(self.data) * 4.0
+        vecs = [np.array([cos(angle), sin(angle)]) for angle in angles4]
+        vec_mean = np.mean(vecs, axis=0)
+        angle = atan2(vec_mean[1], vec_mean[0])/4.0
+        return angle
+
+        
+
 
 def extract_rect(X):
     rect = utils.minimum_bounding_rectangle(X)
@@ -51,7 +75,8 @@ class SquareDetector:
         self.sub = rospy.Subscriber("/cloud2d_projected", Projected, self.callback)
         self.pub = rospy.Publisher("/square_pose", Point, queue_size = 1)
         self.pub_img = rospy.Publisher("/tbtop_debug_image", Image)
-        self.s_queue = MyQueue(n_ave)
+        self.x_queue = MyQueue(n_ave, 2)
+        self.angle_queue = S1Queue(6)
         self.br = tf.TransformBroadcaster()
 
     def callback(self, msg):
@@ -62,15 +87,19 @@ class SquareDetector:
         isValid = lambda area: area > 0.006 and area < 0.009
 
         if isValid(area):
-            s = np.array([center[0], center[1], angle])
-            self.s_queue.push(s)
-            s_mean = self.s_queue.mean()
+            #s = np.array([center[0], center[1], angle])
+            self.x_queue.push(center)
+            self.angle_queue.push(angle)
+            x_mean = self.x_queue.mean()
+            angle_mean = self.angle_queue.mean() + pi/2
+            if angle_mean > pi/2:
+                angle_mean = angle_mean - pi/2
 
-            s_mean_pub= Point(x = s_mean[0], y = s_mean[1], z = s_mean[2])
-            self.pub.publish(s_mean_pub)
+            x_mean_pub= Point(x = x_mean[0], y = x_mean[1], z = angle_mean)
+            self.pub.publish(x_mean_pub)
 
-            rot = tf.transformations.quaternion_from_euler(0, 0.0, s_mean[2])
-            trans = [s_mean[0], s_mean[1], 0.723]
+            rot = tf.transformations.quaternion_from_euler(0, 0.0, angle_mean)
+            trans = [x_mean[0],x_mean[1], 0.723]
             self.br.sendTransform(trans, rot, rospy.Time.now(), "can", "base_link")
 
 if __name__=='__main__':
